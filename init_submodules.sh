0e3bd732f6c2f4754605bbdab331e91f938d2678
Ignatella <ignatellasecond@gmail.com>
2024-05-26 15:30:21 +0200

    Add: ignore untracked files in submodule

--- init_submodules.sh
+++ init_submodules.sh
@@ -2,3 +2,8 @@
 
 git submodule init
 git submodule update
+
+# Ignore untracked files in submodule
+# untracked
+# Only untracked files in submodules will be ignored. Committed differences and modifications to tracked files will show up.
+git config submodule.document-generation/stylegan3.ignore untracked
