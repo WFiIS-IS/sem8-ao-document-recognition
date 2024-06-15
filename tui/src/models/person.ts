c0fecce7ad5eeb7e4c86b0da2a5aa6fe7ccaf74d
Alan <alanguzek@gmail.com>
2024-06-15 22:31:59 +0200

    Person type changed

--- tui/src/models/person.ts
+++ tui/src/models/person.ts
@@ -1,7 +1,7 @@
 export type Person = {
-    pesel: string
-    first_name: string
-    last_name: string
-    date_of_birth: Date
-    sex: "Male" | "Female"
-}
+  pesel: string;
+  first_name: string;
+  last_name: string;
+  // date_of_birth: Date
+  // sex: "Male" | "Female"
+};
